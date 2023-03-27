3c884027ee3a5964c63ef392dac6973493adf489
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-03-27 15:08:37 +0530

    Update 437. Path Sum III.java

--- Medium/437. Path Sum III.java
+++ Medium/437. Path Sum III.java
@@ -1,15 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-  public int pathSum(TreeNode root, int sum) {
-    if (root == null)
-      return 0;
-    return dfs(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
-  }
+    public int pathSum(TreeNode root, int targetSum) {
+        return helper(root,(long) targetSum, 0);
+        
+    }
+    static int helper(TreeNode root, long targetSum, int count) {
+        if (root == null) {
+            return count;
+        }
+        count = dfs(root, targetSum, 0, count);
+        count = helper(root.left, targetSum, count);
+        count = helper(root.right, targetSum, count);
 
-  private int dfs(TreeNode root, int sum) {
-    if (root == null)
-      return 0;
-    return (sum == root.val ? 1 : 0) +   //
-        dfs(root.left, sum - root.val) + //
-        dfs(root.right, sum - root.val);
-  }
+
+        return count;
+    }
+
+    static int dfs(TreeNode root, long targetSum, long sum, int count) {
+        if (root == null) return count;
+        sum += root.val;
+        if (sum == targetSum) {
+            ++count;
+        }
+
+        count = dfs(root.left, targetSum, sum, count);
+        count = dfs(root.right, targetSum, sum, count);
+        sum -= root.val;
+
+        return count;
+
+
+    }
 }
