6e184cc89ff1ce1e8cef869275ebd8c97746c432
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-02-26 12:54:59 +0530

    Update 100. Same Tree.java

--- Easy/100. Same Tree.java
+++ Easy/100. Same Tree.java
@@ -1,9 +1,25 @@
 class Solution {
-  public boolean isSameTree(TreeNode p, TreeNode q) {
-    if (p == null || q == null)
-      return p == q;
-    return p.val == q.val &&          //
-        isSameTree(p.left, q.left) && //
-        isSameTree(p.right, q.right);
-  }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //  Base case
+//         if(p==null && q==null){
+//             return true;
+//         }
+//         if(p==null || q==null){
+//             return false;
+//         }
+//         if(p.val != q.val){
+//             return false;
+//         }
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
+
+
+
+// OR
+
+if(p==null && q==null) return true;
+if(p==null || q==null) return false;
+return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && p.val==q.val;
+    }
 }
