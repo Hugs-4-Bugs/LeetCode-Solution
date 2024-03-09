760360bd1cba52b6a4f1625464f8e63b4d8b1285
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-03-09 21:39:02 +0530

    Update 2540. Minimum Common Value.java

--- Easy/2540. Minimum Common Value.java
+++ Easy/2540. Minimum Common Value.java
@@ -1,17 +1,17 @@
 class Solution {
-  public int getCommon(int[] nums1, int[] nums2) {
-    int i = 0; // nums1's index
-    int j = 0; // nums2's index
-
-    while (i < nums1.length && j < nums1.length) {
-      if (nums1[i] == nums2[j])
-        return nums1[i];
-      if (nums1[i] < nums2[j])
-        ++i;
-      else
-        ++j;
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return -1;
     }
-
-    return -1;
-  }
 }
