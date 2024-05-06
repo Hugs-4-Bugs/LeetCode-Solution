1be08a7faea10d72174dea77e9a8d0b13514e1e0
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-05-06 19:51:47 +0530

    Update 2487. Remove Nodes From Linked List.java

--- Medium/2487. Remove Nodes From Linked List.java
+++ Medium/2487. Remove Nodes From Linked List.java
@@ -6,3 +6,32 @@ class Solution {
     return head.next != null && head.val < head.next.val ? head.next : head;
   }
 }
+
+
+
+
+// OR
+
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        ListNode cur = head;
+        Stack<ListNode> stack = new Stack<>();
+        
+        while (cur != null) {
+            while (!stack.isEmpty() && stack.peek().val < cur.val) {
+                stack.pop();
+            }
+            stack.push(cur);
+            cur = cur.next;
+        }
+        
+        ListNode nxt = null;
+        while (!stack.isEmpty()) {
+            cur = stack.pop();
+            cur.next = nxt;
+            nxt = cur;
+        }
+        
+        return cur;
+    }
+}
