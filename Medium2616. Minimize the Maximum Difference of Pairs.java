d90a7de90f9488e9478cc7c67418efbeb8290774
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-10-11 12:37:00 +0530

    Update Medium2616. Minimize the Maximum Difference of Pairs.java

--- Medium2616. Minimize the Maximum Difference of Pairs.java
+++ Medium2616. Minimize the Maximum Difference of Pairs.java
@@ -17,6 +17,7 @@ class Solution {
     return l;
   }
 
+  
   // Returns the # of pairs that can be obtained if the difference between each
   // pair <= `maxDiff`.
   private int numPairs(int[] nums, int maxDiff) {
