3e1747faa108723ddd93365d5eaae185dbf9f88b
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2025-01-09 23:45:56 +0530

    Update 1480. Running Sum of 1d Array.java

--- Easy/1480. Running Sum of 1d Array.java
+++ Easy/1480. Running Sum of 1d Array.java
@@ -1,9 +1,34 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-    int[] ans = new int[nums.length];
-    ans[0] = nums[0];
-    for (int i = 1; i < nums.length; i++)
-        ans[i] = ans[i-1] + nums[i];
-    return ans;
+        int n = nums.length; 
+        int[] sum = new int[n];
+        if(n > 0)
+
+        sum[0] = nums[0];
+        for(int i=1; i<n; i++){
+            sum[i] = sum[i-1] + nums[i];
+        }
+
+        return sum;
     }
 }
+
+
+
+
+
+
+
+
+// OR
+
+
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//     int[] ans = new int[nums.length];
+//     ans[0] = nums[0];
+//     for (int i = 1; i < nums.length; i++)
+//         ans[i] = ans[i-1] + nums[i];
+//     return ans;
+//     }
+// }
